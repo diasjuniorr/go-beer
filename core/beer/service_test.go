1d1067c004b7c596dacd6863b431a893a4274f4f
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-04-18 23:05:45 -0300

    adds test for get method

--- core/beer/service_test.go
+++ core/beer/service_test.go
@@ -2,10 +2,13 @@ package beer_test
 
 import (
 	"database/sql"
+	"os"
 	"testing"
 
 	"github.com/jotajay/beer_api/core/beer"
 )
+
+var db *sql.DB
 
 func TestStore(t *testing.T) {
 	b := &beer.Beer{
@@ -17,14 +20,21 @@ func TestStore(t *testing.T) {
 
 	db, err := sql.Open("sqlite3", "../../data/beer_test.db")
 	if err != nil {
-		t.Fatalf("failed to initialize db: %v", err)
+		os.Exit(1)
 	}
+
+	db.Exec("delete from beer")
 
 	s := beer.NewService(db)
 
 	err = s.Store(b)
 	if err != nil {
 		t.Fatalf("failed to store beer: %v", err)
+	}
+
+	beer, err := s.Get(1)
+	if beer.Name != "Heineken" {
+		t.Fatalf("failed to match name: %v", beer)
 	}
 	t.Logf("beer stored successfully: %v", b)
 }
