fcdc6df21c04f03b0b54711c6aa20ac52f1f49bb
Dias Junior <reinaldo.diasjunior@gmail.com>
2021-04-20 13:45:28 -0300

    feat(handlers): adds handlers for post and get beer

--- api/main.go
+++ api/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/jotajay/beer_api/api/handlers"
 	"github.com/jotajay/beer_api/core/beer"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -23,7 +23,8 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/v1/beers", getAll(service)).Methods("GET")
+	r.HandleFunc("/v1/beers", handlers.GetAllBeers(service)).Methods("GET")
+	r.HandleFunc("/v1/beers", handlers.PostBeer(service)).Methods("POST")
 	http.Handle("/", r)
 
 	srv := &http.Server{
@@ -37,20 +38,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to start server. %v", err)
 	}
-}
-
-func getAll(service beer.UseCase) func(http.ResponseWriter, *http.Request) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("chegou")
-		beers, err := service.GetAll()
-		if err != nil {
-			fmt.Printf("Error getting beer: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-
-		for b, _ := range beers {
-
-			fmt.Printf("beer: %v", b)
-		}
-	})
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/urfave/negroni v1.0.0 // indirect
 )
